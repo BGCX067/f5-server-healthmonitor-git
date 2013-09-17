c7e22e1e9d729651c83de3584f0ffdc1e0e3104a
lucas <lucas@Ubuntu.(none)>
2013-09-16 23:46:38 -0700

    Second commit- Added comments, changed up the client and server to send back and forth instead of one way.

--- SimpleChat/src/com/example/simplechat/MainActivity.java
+++ SimpleChat/src/com/example/simplechat/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.simplechat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
@@ -20,57 +18,79 @@ public class MainActivity extends Activity {
     private EditText send_area;
     private Button btn;
     
+    private Socket socket;
+    private static final int PORT = 40001;
+    private static String S_IP = "10.0.2.2";
     
+    /****************************************************************
+	* Author: Lucas Walter						           
+	* Function Name: protected void onCreate(Bundle savedInstanceState)
+	* Description: Creates the Activity to show on the Screen, initializes
+	* 	objects
+	* Preconditions: N/A
+	* Postconditions: Thread created for the client, variables initialized
+	****************************************************************/
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		msg_area = (TextView)findViewById(R.id.tv_chat);
 		send_area = (EditText)findViewById(R.id.tb_msg);
 		btn = (Button)findViewById(R.id.btn_send);
+		new Thread(new ClientThread()).start();
 		
 	
 		btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Thread cThread = new Thread(new Client());
-				cThread.start();
+				//if not connected to the socket...
+				if(!socket.isConnected()) {
+					new Thread(new ClientThread()).start();
+				}
+				
+				try {
+					//get the message to send to the server
+					String msg = send_area.getText().toString();
+					//init the outstream to the socket
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					//init the input stream from the server
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					//send the string to the socket
+					out.println(msg);
+					//out.flush();
+					//get the response from the server
+					String server_msg = in.readLine();
+					//Log.d("server_msg", server_msg);
+					//update the text field with the response
+					msg_area.append(server_msg);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 	}
 	
-	public class Client implements Runnable{
+	//If the app is stopped, attempt to close the socket
+	protected void onStop() {
+		super.onStop();
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Inits the socket
+	public class ClientThread implements Runnable {
 		public void run() {
-			Socket socket;
-			BufferedReader in;
-			PrintWriter out;
 			try {
-				InetAddress server_addr = InetAddress.getByName("10.0.2.2");
-				socket = new Socket(server_addr, 40001);
-
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				out = new PrintWriter(socket.getOutputStream(), true);
-				
-				String msg = send_area.getText().toString();
-//				/Log.d(msg, msg);
-				if(!msg.equals("exit")) {
-					out.println(msg);
-					out.flush();
-				}
-				else {
-					if(in != null) {
-						in.close();
-					}
-					if(out != null) {
-						out.close();
-					}
-					if(socket != null) {
-						socket.close();
-					}
-				}
-			}
-			catch(UnknownHostException e) {
+				InetAddress server_addr = InetAddress.getByName(S_IP);
+				socket = new Socket(server_addr, PORT);
+			} catch (UnknownHostException e) {
 				e.printStackTrace();
-			}
-			catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
--- SimpleChat/src/com/example/simplechat/Server.java
+++ SimpleChat/src/com/example/simplechat/Server.java
@@ -3,6 +3,7 @@ package com.example.simplechat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,22 +11,42 @@ public class Server {
 	private static ServerSocket server_sock;
 	private static Socket client_sock;
 	private static BufferedReader in;
-	
+	private PrintWriter out;
+
+	/****************************************************************
+	* Author: Lucas Walter						           
+	* Function Name: public Server(int port)
+	* Description: Public constructor for a server to bind and listen to a socket
+	* Preconditions: int port representing the port number to bind to
+	* Postconditions:  Initializes a Server object
+	****************************************************************/
 	public Server(int port) {
 		try {
 			System.out.println("Binding to port " + port);
+			//bind to the given port
 			server_sock = new ServerSocket(port);
 			System.out.println("Server started: " + server_sock);
 			System.out.println("Waiting for a client");
+			//wait for a client to connect
 			client_sock = server_sock.accept();
 			System.out.println("Client accepted: " + client_sock);
+			//open the bufferedreader to get data in the socket
 			open();
 			boolean done = false;
+			//while the user hasn't entered in exit to quit the connection
 			while(!done) {
 				try {
 					//System.out.println("getting message...");
+					//get the data from the socket
 					String line = in.readLine();
+					//print it out on the Server command line
 					System.out.println("Client said: " + line);
+					//generate response to send back
+					String response = "You said: " + line;
+					System.out.println("Sending to client: " + response);
+					out.println(response);
+					//out.flush();
+					//update setinel
 					done = line.equals("exit");
 				}
 				catch(IOException e) {
@@ -39,14 +60,39 @@ public class Server {
 		}
 	}
 	
+	/****************************************************************
+	* Author: Lucas Walter						           
+	* Function Name: public void open() throws IOException
+	* Description: Initializes an InputStreamReader at the socket to read in data
+	* Preconditions: N/A
+	* Postconditions:  Initializes an InputStreamReader at the bound socket
+	****************************************************************/
 	public void open() throws IOException {
 		in = new BufferedReader(new InputStreamReader(client_sock.getInputStream()));
+		out = new PrintWriter(client_sock.getOutputStream(), true);
 	}
 	
+	/****************************************************************
+	* Author: Lucas Walter						           
+	* Function Name: public void close() throws IOException
+	* Description: Closes the socket and the InputStreamReader when the client has 
+	* 	disconnected
+	* Preconditions: N/A
+	* Postconditions: Socket and InputStreamReader closed
+	****************************************************************/
 	public void close() throws IOException {
 		if(client_sock != null) { client_sock.close(); }
 		if(in != null) { in.close(); }
 	}
+	
+	/****************************************************************
+	* Author: Lucas Walter						           
+	* Function Name: public static main(String args[])
+	* Description: Creates a server object from command line
+	* Preconditions: N/A
+	* Postconditions: Server data member is allocated and bound to a socket
+	* 	ready for a client connection
+	****************************************************************/
 	public static void main(String[] args) {
 		Server server = null;
 		if(args.length != 1) {
